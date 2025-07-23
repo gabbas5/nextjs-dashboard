c9b927e7fddff034a7f7fff71c51ea2c2737ea6c
gabbas5 <gabbas5@hotmail.com>
2025-07-23 16:50:22 +0500

    first commit in nextjs-15_latest

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Page = () => {
+  return (
+    <div>customer Page</div>
+  )
+}
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Page = () => {
+  return (
+    <div>invoices Page</div>
+  )
+}
+
+export default Page;
